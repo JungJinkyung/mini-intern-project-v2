b0b31eb025323bf1cd661dec0b3e98f350cae8e4
jkea1 <jinkyung@weirdsector.co.kr>
2024-07-02 17:51:09 +0900

    init: 기본 설정

---
+++ client/preset.js
@@ -0,0 +1,105 @@
+module.exports = {
+  // darkMode: ["class", "dark-mode-no-use"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "16px"
+      }
+    },
+    colors: {
+      black: {
+        DEFAULT: "#000"
+      },
+      white: {
+        DEFAULT: "#FFF"
+      },
+      lightgray: {
+        300: "#E1E1E8",
+        900: "#2B2D36"
+      },
+      gray: {
+        DEFAULT: "#808080",
+        50: "#F9F9F9",
+        100: "#EEEEEE",
+        200: "#E1E1E1",
+        300: "#CFCFCF",
+        400: "#B8B7B7",
+        500: "#808080",
+        600: "#646464",
+        700: "#414040 ",
+        800: "#272727"
+      },
+      red: {
+        DEFAULT: "#EB4852",
+        MAIN: "#EE3918",
+        100: "#FFCACD",
+        200: "#F9AAAF",
+        300: "#F9AAAF",
+        400: "#F6626B",
+        500: "#EB4852",
+        600: "#EC2C38",
+        700: "#DE0E1B"
+      },
+      pink: {
+        // DEFAULT: '',
+        100: "#FFE9E3",
+        200: "#FFD9D0",
+        300: "#FFBAAA",
+        400: "#FF9C85",
+        500: "#FF8669",
+        600: "#FF6F4E"
+      },
+      orange: {
+        DEFAULT: "#FFB300",
+        100: "#FFF7E6",
+        200: "#FFF1D1",
+        300: "#FFE39F",
+        400: "#FFC845",
+        500: "#FFB300",
+        600: "#FFA115",
+        700: "#FC9013",
+        800: "#F58014",
+        900: "#E36D00"
+      },
+      green: {
+        // DEFAULT: '',
+        100: "#DAF6E1",
+        200: "#BFF2CB",
+        300: "#96E4A9",
+        400: "#73D089",
+        500: "#64C179",
+        600: "#3A9F51"
+      },
+      cyan: {
+        // DEFAULT: '',
+        100: "#E3F2EF",
+        200: "#AEE0DD",
+        300: "#74CFD4",
+        400: "#3AB3BA",
+        500: "#2095A7",
+        600: "#0D8092"
+      },
+      blue: {
+        // DEFAULT: '',
+        100: "#DAE9FF",
+        200: "#B4CDFF",
+        300: "#8AB2FF",
+        400: "#6499FF",
+        500: "#4D8AFF",
+        600: "#3174F2"
+      },
+      purple: {
+        // DEFAULT: '',
+        100: "#E9E1FF",
+        200: "#D4C6FF",
+        300: "#C4ACFC",
+        400: "#9B7AFE",
+        500: "#7A52F2",
+        600: "#5E34DC"
+      }
+    },
+    fontFamily: {},
+    extend: {}
+  },
+  plugins: []
+};
