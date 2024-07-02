193a935e271c283f65f35679c63d116dc732f4f7
jkea1 <jinkyung@weirdsector.co.kr>
2024-07-02 17:57:20 +0900

    feat: 댓글 리스트 아이템 생성

---
+++ client/src/app/components/listItems/CommentListItem.tsx
@@ -0,0 +1,15 @@
+import { Comment as commentI } from "@/types/comment";
+
+type props = {
+  comment: commentI;
+};
+
+export default function Comment({ comment }: props) {
+  return (
+    <div className="flex flex-col gap-y-4 justify-center h-[240px] border-t-[1px] border-t-[#E1E1E1]">
+      <div className="text-xl font-bold">{comment.nickname}</div>
+      <p className="text-xl">{comment.commentText}</p>
+      <div className="text-gray-400">{comment.createdAt}</div>
+    </div>
+  );
+}
