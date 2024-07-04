e29895cca93a5198d8dd9019a7e4d18bd8b5a0e8
jkea1 <jinkyung@weirdsector.co.kr>
2024-07-04 09:22:18 +0900

    feat: login 페이지 에러 메세지 설정 및 제출 기능 생성

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,30 +1,56 @@
+"use client";
+
 import Button from "@/app/components/common/Button";
+import LoginInput from "@/app/components/inputs/login-input";
 import Link from "next/link";
+import { useState } from "react";
+import validateLoginInputs from "../utils/validate-login-inputs";
 
 export default () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errors, setErrors] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(email, password);
+
+    if (validateLoginInputs({ setErrors, email, password })) {
+      try {
+        /* 로그인 api 처리 로직... */
+      } catch (error) {
+        alert("회원가입에 실패했습니다!");
+      }
+    }
+  };
+
   return (
     <div className="my-0 h-[711px] flex flex-col justify-center items-center">
-      <h1 className="text-[#040404] text-[32px] font-bold mb-4">로그인</h1>
-      <form /* onSubmit={handleLogin} */>
+      <h1 className="text-gray-800 text-[32px] font-bold mb-4">로그인</h1>
+      <form onSubmit={handleLogin}>
         <div className="flex flex-col gap-y-2">
-          <input
-            className="w-[383px] h-[52px] border-[1px] rounded border-[#E1E1E1] p-5"
+          <LoginInput
             type="email"
+            state={email}
+            setState={setEmail}
             placeholder="이메일 주소"
-            /* onChange={handleEmail} */
           />
-          {/* {errors.email && (
+          {errors.email && (
             <p className="text-red-500 text-sm">{errors.email}</p>
-          )} */}
-          <input
-            className="w-[383px] h-[52px] border-[1px] rounded border-gray-200 p-5"
+          )}
+          <LoginInput
             type="password"
+            state={password}
+            setState={setPassword}
             placeholder="비밀번호 입력"
-            /* onChange={handlePassword} */
           />
-          {/* {errors.password && (
+          {errors.password && (
             <p className="text-red-500 text-sm">{errors.password}</p>
-          )} */}
+          )}
         </div>
         <Button color="black" size="xl" className="my-3">
           로그인
