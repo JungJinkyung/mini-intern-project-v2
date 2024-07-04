b1b5142c48a5199b1b39446626708b6fb0314493
jkea1 <jinkyung@weirdsector.co.kr>
2024-07-04 09:23:43 +0900

    feat: 회원 가입 페이지 에러 메세지 생성 및 제출 기능 생성

--- client/src/app/(auth)/registration/page.tsx
+++ client/src/app/(auth)/registration/page.tsx
@@ -1,73 +1,105 @@
+"use client";
+
 import Button from "@/app/components/common/Button";
+import RegistrationInput from "@/app/components/inputs/registration-input";
+import { useState } from "react";
+import validateRegistrationInputs from "../utils/validate-registration-inputs";
+import styles from "./page.module.css";
 
 export default () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmedPassword, setConfirmedPassword] = useState<string>("");
+  const [nickname, setNickname] = useState<string>("");
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+    confirmedPassword: "",
+    nickname: ""
+  });
+
+  const handleRegistrationInput = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    console.log(email, password, confirmedPassword, nickname);
+
+    if (
+      validateRegistrationInputs({
+        setErrors,
+        email,
+        password,
+        nickname,
+        confirmedPassword
+      })
+    ) {
+      try {
+        /* 로그인 api 처리 로직... */
+      } catch (error) {
+        alert("회원가입에 실패했습니다!");
+      }
+    }
+  };
+
   return (
     <div>
       <div className="h-[1111px] flex flex-col justify-center items-center">
         <h1 className="text-[#040404] text-[32px] font-bold mb-16">회원가입</h1>
-        <form className="flex flex-col gap-y-6" /* onSubmit={handleSignUp} */>
+        <form
+          className="flex flex-col gap-y-6"
+          onSubmit={handleRegistrationInput}>
           <div>
             <h2 className="text-base font-bold mb-2">이메일</h2>
-            <input
-              className="w-[383px] h-[52px] border-[1px] rounded border-[#E1E1E1] p-5"
+            <RegistrationInput
               type="email"
-              /* onChange={handleEmail} */
+              state={email}
+              setState={setEmail}
+              placeholder="이메일을 입력해주세요."
             />
-            {/* {errors.email && (
-            <p className='text-red-500 text-sm relative top-2'>
-              {errors.email}
-            </p>
-          )} */}
+            {errors.email && (
+              <p className={`${styles.error}`}>{errors.email}</p>
+            )}
           </div>
           <div>
             <h2 className="text-base font-bold mb-2">비밀번호</h2>
             <div className="flex flex-col gap-y-2">
-              <input
-                className="w-[383px] h-[52px] border-[1px] rounded border-[#E1E1E1] p-5"
+              <RegistrationInput
                 type="password"
                 placeholder="8자 이상, 영문자, 숫자, 특수기호중 2가지 조합"
-                /* onChange={handlePassword} */
+                state={password}
+                setState={setPassword}
               />
-              {/* {errors.password && (
-              <p className='text-red-500 text-sm'>{errors.password}</p>
-            )} */}
-              <input
-                className="w-[383px] h-[52px] border-[1px] rounded border-[#E1E1E1] p-5"
+              {errors.password && (
+                <p className={`${styles.error} mb-2`}>{errors.password}</p>
+              )}
+              <RegistrationInput
                 type="password"
                 placeholder="비밀번호를 다시 입력해주세요"
-                /* onChange={handleConfirmPassword} */
+                state={confirmedPassword}
+                setState={setConfirmedPassword}
               />
-              {/* {errors.confirmPassword && (
-              <p className='text-red-500 text-sm'>{errors.confirmPassword}</p>
-            )} */}
+              {errors.confirmedPassword && (
+                <p className={`${styles.error}`}>{errors.confirmedPassword}</p>
+              )}
             </div>
           </div>
           <div>
             <h2 className="text-base font-bold mb-2">닉네임</h2>
-            <input
-              className="w-96 h-[52px] border-[1px] rounded border-[#E1E1E1] p-5"
+            <RegistrationInput
               type="text"
-              // onChange={handleNickname}
+              placeholder="닉네임을 입력해주세요."
+              state={nickname}
+              setState={setNickname}
             />
-            {/* {errors.nickname && (
-            <p className='text-red-500 text-sm relative top-2'>
-              {errors.nickname}
-            </p>
-          )} */}
+            {errors.nickname && (
+              <p className={`${styles.error}`}>{errors.nickname}</p>
+            )}
           </div>
           <div className="w-[384px] h-[75px] bg-[#F9F9F9] flex justify-center items-center text-base gap-x-2 rounded-lg my-2">
-            {/* <Checkbox
-            initialChecked={agree}
-            onChange={(checked) => setAgree(checked)}
-          /> */}
             <h3>개인정보 처리방침 / 데이터 활용 동의</h3>
             <h3 className="text-[#808080]">(필수)</h3>
           </div>
-          {/* {errors.agree && (
-          <p className='text-red-500 text-sm relative bottom-6'>
-            {errors.agree}
-          </p>
-        )} */}
           <div className="w-[100%] flex justify-center">
             <Button color="black" size="md" className="mt-12">
               회원가입
