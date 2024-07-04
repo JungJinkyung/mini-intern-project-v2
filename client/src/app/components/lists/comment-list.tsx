0fd9e8f9dcb244bc5b89e8434beda2263affe1e9
jkea1 <jinkyung@weirdsector.co.kr>
2024-07-04 09:30:53 +0900

    feat: lists 컴포넌트 생성

---
+++ client/src/app/components/lists/comment-list.tsx
@@ -0,0 +1,18 @@
+import Comment from "@/app/components/list-items/comment-list-item";
+import { Fragment } from "react/jsx-runtime";
+
+type props = {
+  comments: Comment[];
+};
+
+export default ({ comments }: props) => {
+  return (
+    <>
+      {comments?.map((comment, idx) => (
+        <Fragment key={idx}>
+          <Comment comment={comment} />
+        </Fragment>
+      ))}
+    </>
+  );
+};
