f4c8cbc2e3bbf02e2b204dbccb0e1eb8919cabd8
jkea1 <jinkyung@weirdsector.co.kr>
2024-07-15 14:27:21 +0900

    chore: 코드스타일 적용

--- client/src/app/components/tags/hash-tag.tsx
+++ client/src/app/components/tags/hash-tag.tsx
@@ -5,7 +5,7 @@ type props = {
 export default ({ children = "" }: props) => {
   return (
     <div 
-      className="bg-gray-100 h-8 p-4 rounded-full font-medium text-base flex flex-col justify-center"
+      className={"bg-gray-100 h-8 p-4 rounded-full font-medium text-base flex flex-col justify-center"}
     >
       {`# ${children}`}
     </div>
