505de3f4b45c4cc4d0c4a0e6d8c90e1449482d71
jkea1 <jinkyung@weirdsector.co.kr>
2024-07-18 21:46:22 +0900

    refector : 앞에 window 붙이기

--- client/src/utils/token.tsx
+++ client/src/utils/token.tsx
@@ -2,14 +2,14 @@ const ACCESS_TOKEN_KEY = 'basic-token-key';
 const REFRESH_TOKEN_KEY = 'refresh-token-key';
 
 export const saveToken = (token: any) => {
-  localStorage.setItem(ACCESS_TOKEN_KEY, token.accessToken);
+  window.localStorage.setItem(ACCESS_TOKEN_KEY, token.accessToken);
   // localStorage.setItem(REFRESH_TOKEN_KEY, token.refreshToken);
 };
 
 export const getToken = () => {
-  return localStorage.getItem(ACCESS_TOKEN_KEY);
+  return window.localStorage.getItem(ACCESS_TOKEN_KEY);
 };
 
 export const clearToken = () => {
-  localStorage.removeItem(ACCESS_TOKEN_KEY);
+  window.localStorage.removeItem(ACCESS_TOKEN_KEY);
 };
